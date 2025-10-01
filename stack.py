d67e8d1cfda27d2a5d1c1242fff3969454405c6f
67160244-jpg <67160244@go.buu.ac.th>
2025-10-01 18:40:20 +0700

    stack.py

--- stack.py
+++ stack.py
@@ -2,18 +2,34 @@ class Stack:
     def __init__(self):
         self.items = []
 
-    def push(self,item):
+    def push(self, item):
         self.items.append(item)
 
     def pop(self):
         if self.is_empty():
-            
+            return "Stack is empty!"
+        return self.items.pop()
 
     def peek(self):
+        if self.is_empty():
+            return "Stack is empty!"
+        return self.items[-1]
 
     def is_empty(self):
+        return len(self.items) == 0
 
     def size(self):
+        return len(self.items)
+
+s = Stack()
+
+print("Is empty?", s.is_empty())
+
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(4)
+s.push(5)
 
 print("Size after push:", s.size())
 print("Top element:", s.peek())
